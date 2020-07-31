22d1889912eb9ddd83f521c5d186004fcaa6ff2d
Abhinav Kumar <43418540+abhiinavkumarr@users.noreply.github.com>
2020-07-31 12:02:42 +0530

    Add files via upload

---
+++ websiteblocker[1].py
@@ -0,0 +1,26 @@
+import time
+from datetime import datetime as dt
+redirect="127.0.0.1"
+websites=["www.youtube.com","youtube.com", "www.facebook.com", "facebook.com"]
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,9,50) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,9,51):
+        
+        print ("WORKING HOUR YOU ARE NOT ALLOWED TO USE THIS SITE.......")
+        with open("website.txt",'r+') as file:
+            content = file.read()
+            for site in websites:
+                if site in content:
+                    pass
+                else:
+                    file.write(redirect+" "+site+"\n")
+        time.sleep(10)
+    else:
+        with open("website.txt",'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(site in line for site in websites):
+                    file.write(line)
+                file.truncate()
+            print ("THANKS NOW YOU CAN USE THIS SITE....")
+        time.sleep(10)
